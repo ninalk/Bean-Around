de040cb4d73d987e0e1aa388354b0c892550ebd3
Nina Kim <neenerskim@gmail.com>
2021-03-15 11:10:56 -0700

    change splash page to view/index instead of view/user/index.ejs

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const passport = require('passport');
 
 // The root route renders our only view
 router.get('/', function(req, res) {
+  console.log(req.user, ' this is the user')
   // Where do you want to go for the root route
-  res.redirect('/users');
+  res.render('index');
 });
 
 // Google OAuth login route
@@ -17,15 +18,15 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '/users', // where do you want the client to go after you login 
-    failureRedirect : '/users' // where do you want the client to go if login fails
+    successRedirect : '/beans', // where do you want the client to go after you login 
+    failureRedirect : '/' // where do you want the client to go if login fails
   }
 ));
 
 // OAuth logout route
 router.get('/logout', function(req, res){
   req.logout();
-  res.redirect('/users');
+  res.redirect('/');
 });
 
 module.exports = router;
